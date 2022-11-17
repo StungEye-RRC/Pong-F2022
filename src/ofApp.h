57c414ea35add2b5a3710864f49a8f1d6974a46f
Kyle Geske <stungeye@gmail.com>
2022-11-17 14:12:45 -0600

    Removed unneeded event methods from the cpp and h files.

--- src/ofApp.h
+++ src/ofApp.h
@@ -2,23 +2,29 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key) override;
+	void keyReleased(int key) override;
+	void windowResized(int w, int h) override;
+
+private:
+	const int canvasWidth{800}, canvasHeight{500};
+
+	bool startRally{true};
+	bool p1Serves{ofRandom(0, 100) < 50};
+
+	bool p1UpPressed{false}, p1DownPressed{false}, p2UpPressed{false}, p2DownPressed{false};
+
+	float ballXPosition{0.0f}, ballYPosition{0.0f};
+	float ballXSpeed{0.0f}, ballYSpeed{0.0f};
+
+	float p1YPosition{0.0f}, p2YPosition{0.0f};
+
+	short p1Score{0}, p2Score{0};
 };
