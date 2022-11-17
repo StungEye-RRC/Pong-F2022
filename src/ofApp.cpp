6b48c39517ac9e53942d29167862e4cd0727c9d5
Kyle Geske <stungeye@gmail.com>
2022-11-17 14:31:05 -0600

    Sketched out the required logic in comments.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,20 +9,28 @@ void ofApp::setup() {
 
 //--------------------------------------------------------------
 void ofApp::update() {
-
+	// RALLY RESTART (Conditional on bool)
+	// MOVE PADDLES (Conditional on user input)
+	// MOVE BALL
+	// BALL BOUNCE ON EDGE (Conditional y position of ball)
+	// BALL BOUNCE ON PADDLE 1 (Conditional y pos & x pos of ball in relation to P1)
+	// BALL BOUNCE ON PADDLE 2 (Conditional y pos & x pos of ball in relation to P2)
+	// CHECK FOR POINT SCORED (Conditional x pos of ball)
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-
+	// DRAW SCORES
+	// DRAW PADDLES
+	// DRAW BALL
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
-
+	// SET USER INPUT BOOLS 
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key) {
-
+	// UNSET USER INPUT BOOLS
 }
