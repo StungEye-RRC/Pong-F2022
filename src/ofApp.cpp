97108f46d2979c74a027fff268f2cfdf4fb8dcb3
Kyle Geske <stungeye@gmail.com>
2022-11-20 00:03:18 -0600

    Refactor into separate classes.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,89 +3,18 @@
 //--------------------------------------------------------------
 void ofApp::setup() {
 	ofSetFrameRate(100);
-	ofSetWindowShape(canvasWidth, canvasHeight);
+	ofSetWindowShape(800, 500);
 	ofSetWindowPosition(900, 200);
 	ofSetRectMode(OF_RECTMODE_CENTER);
+	pongGame.setup(PongGame::GameType::playerVsPlayer);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-	// RALLY RESTART (Conditional on bool)
-	if (startRally) {
-		startRally = false;
-		ballXPosition = ofGetWidth() / 2.0f;
-		ballYPosition = p2YPosition = p1YPosition = ofGetHeight() / 2.0f;
-
-		ballXSpeed = p1Serves ? 300 : -300;
-		ballYSpeed = ofRandom(0, 200) - 100; // Random from -100 to 100
-	}
-
-	// MOVE BALL (Delta Time Aware)
-	ballXPosition += ballXSpeed * ofGetLastFrameTime();
-	ballYPosition += ballYSpeed * ofGetLastFrameTime();
-
-	// MOVE PADDLES (Conditional on user input. Delta time aware.)
-
-	const double speedChange{300 * ofGetLastFrameTime()};
-
-	if (p1UpPressed) p1YPosition -= speedChange;
-	if (p1DownPressed) p1YPosition += speedChange;
-	if (p2UpPressed) p2YPosition -= speedChange;
-	if (p2DownPressed) p2YPosition += speedChange;
-
-	p1YPosition = ofClamp(p1YPosition, 50, 450);
-	p2YPosition = ofClamp(p2YPosition, 50, 450);
-
-	// BALL BOUNCE ON EDGE (Conditional y position of ball)
-
-	if (ballYPosition <= 10 || ballYPosition >= 490) {
-		ballYSpeed *= -1;
-		ballYPosition = ofClamp(ballYPosition, 10, 490);
-	}
-
-	// BALL BOUNCE ON PADDLE 1 (Conditional y pos & x pos of ball in relation to P1)
-	// BALL BOUNCE ON PADDLE 2 (Conditional y pos & x pos of ball in relation to P2)
-
-	// CHECK FOR POINT SCORED (Conditional x pos of ball)
-	if (ballXPosition < 0) {
-		++p2Score;
-		startRally = true;
-		p1Serves = false;
-	} else if (ballXPosition > canvasWidth) {
-		++p1Score;
-		startRally = true;
-		p1Serves = true;
-	}
+	pongGame.update(ofGetLastFrameTime(), ofGetWidth(), ofGetHeight());
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	// DRAW SCORES
-	ofDrawBitmapString("P1: " + std::to_string(p1Score), 200, 40);
-	ofDrawBitmapString("P2: " + std::to_string(p2Score), 550, 40);
-
-	// DRAW PADDLES
-	ofDrawRectangle(50, p1YPosition, 20, 100);
-	ofDrawRectangle(750, p2YPosition, 20, 100);
-
-	// DRAW BALL
-	ofDrawRectangle(ballXPosition, ballYPosition, 20, 20);
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-	// SET USER INPUT BOOLS
-	if (key == 'w') p1UpPressed = true;
-	if (key == 's') p1DownPressed = true;
-	if (key == OF_KEY_UP) p2UpPressed = true;
-	if (key == OF_KEY_DOWN) p2DownPressed = true;
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-	// UNSET USER INPUT BOOLS
-	if (key == 'w') p1UpPressed = false;
-	if (key == 's') p1DownPressed = false;
-	if (key == OF_KEY_UP) p2UpPressed = false;
-	if (key == OF_KEY_DOWN) p2DownPressed = false;
+	pongGame.draw();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,23 +2,242 @@
 
 #include "ofMain.h"
 
+enum class Edge { top, right, bottom, left, none };
+
+class Sprite {
+protected:
+	float width, height;
+	glm::vec2 position;
+public:
+	Sprite(float xPosition, float yPosition, float width, float height)
+		: width{width}, height{height}, position{xPosition, yPosition} {
+	}
+
+	void warpTo(const glm::vec2& destination) {
+		position = destination;
+	}
+
+	void clampToBoundary(const glm::vec2& min, const glm::vec2& max) {
+		position.x = ofClamp(position.x, min.x + width / 2, max.x - width / 2);
+		position.y = ofClamp(position.y, min.y + height / 2, max.y - height / 2);
+	}
+
+	void draw() const {
+		ofPushStyle();
+		ofSetRectMode(OF_RECTMODE_CENTER);
+		ofDrawRectangle(position.x, position.y, width, height);
+		ofPopStyle();
+	}
+
+	Edge beyondVerticalBounds(float min, float max) const {
+		if (position.x <= min) {
+			return Edge::left;
+		}
+
+		if (position.x > max) {
+			return Edge::right;
+		}
+
+		return Edge::none;
+	}
+
+	std::pair<Edge, float> intersects(Sprite& other) {
+		const ofRectangle myBoundingBox{boundingBox()};
+		const ofRectangle otherBoundingBox{other.boundingBox()};
+
+		if (!myBoundingBox.intersects(otherBoundingBox)) {
+			return {Edge::none, 0.0f};
+		}
+		const ofRectangle intersection = myBoundingBox.getIntersection(otherBoundingBox);
+
+		if (intersection.getHeight() >= intersection.getWidth()) {
+			if (intersection.getX() == otherBoundingBox.getX()) {
+				return {Edge::right, -intersection.getWidth()};
+			}
+
+			return {Edge::left, intersection.getWidth()};
+		}
+
+		if (intersection.getY() == otherBoundingBox.getY()) {
+			return {Edge::bottom, -intersection.getHeight()};
+		}
+		return {Edge::top, intersection.getHeight()};
+	}
+
+	ofRectangle boundingBox() {
+		return {position.x - width / 2, position.y - height / 2, width, height};
+	}
+};
+
+class MotionSprite : public Sprite {
+private:
+	glm::vec2 velocity;
+public:
+	MotionSprite(float xPosition, float yPosition, float width, float height,
+	             float xSpeed, float ySpeed)
+		: Sprite{xPosition, yPosition, width, height}, velocity{xSpeed, ySpeed} {
+	}
+
+	// A setter for velocity.
+	void cruiseAt(const glm::vec2& newVelocity) {
+		velocity = newVelocity;
+	}
+
+	// Apply the velocity to the position.
+	void move(float deltaTime) {
+		position += velocity * deltaTime;
+	}
+
+	// This impulse will be applied to the velocity.
+	void accelerate(glm::vec2 impulse) {
+		velocity += impulse;
+	}
+
+	void bounceHorizontalWithEdge(float ceilingY, float floorY) {
+		// Adjust ceiling and floor positions for the ball size.
+		ceilingY += height / 2.0f;
+		floorY -= height / 2.0f;
+
+		if (position.y <= ceilingY || position.y >= floorY) {
+			velocity.y *= -1;
+			position.y = ofClamp(position.y, ceilingY, floorY);
+		}
+	}
+
+	void bounceVerticalWith(Sprite other) {
+		const std::pair<Edge, float> edgeIntersection = intersects(other);
+		const Edge edge = edgeIntersection.first;
+		const float overlap = edgeIntersection.second;
+
+		if (edge == Edge::right || edge == Edge::left) {
+			velocity.x *= -1;
+			position.x += overlap;
+		} else if (edge == Edge::top || edge == Edge::bottom) {
+			// Speed things up slightly.
+			velocity.x *= -1.05f;
+			velocity.y *= -1.05f;
+			position.y += overlap;
+		}
+	}
+};
+
+
 class PongGame {
 public:
 	enum class GameType { playerVsPlayer, playerVsAi };
 
-	PongGame(GameType gameType) : gameType{gameType} {
+	enum class PlayerTurn { player1, player2 };
 
+	void setup(const GameType& gameType) {
+		playerTurn = (ofRandom(100) > 50) ? PlayerTurn::player1 : PlayerTurn::player2;
+		startRally = true;
+
+		if (gameType == GameType::playerVsPlayer) {
+			ofAddListener(ofEvents().keyPressed, this, &PongGame::keyPressedPvP);
+			ofAddListener(ofEvents().keyReleased, this, &PongGame::keyReleasedPvP);
+		}
 	}
 
-	void update(double deltaTime) {
+	void update(double deltaTime, float canvasWidth, float canvasHeight) {
+		if (startRally) {
+			restartRally(canvasWidth, canvasHeight);
+		}
+
+		gamePlay(deltaTime, canvasWidth, canvasHeight);
+		checkForWin(canvasWidth);
 	}
 
 	void draw() {
+		ofDrawBitmapString("P1: " + std::to_string(p1Score), 200, 40);
+		ofDrawBitmapString("P2: " + std::to_string(p2Score), 550, 40);
 
+		p1Paddle.draw();
+		p2Paddle.draw();
+
+		ball.draw();
+	}
+
+	void restartRally(float canvasWidth, float canvasHeight) {
+		startRally = false;
+		const auto horizontalMiddle{canvasWidth / 2.0f};
+		const auto verticalMiddle{canvasHeight / 2.0f};
+
+		// Reposition ball and paddles to center points.
+		ball.warpTo({horizontalMiddle, verticalMiddle});
+		p1Paddle.warpTo({paddleEdgeBuffer, verticalMiddle});
+		p2Paddle.warpTo({canvasWidth - paddleEdgeBuffer, verticalMiddle});
+
+		// Pick random Y speed from a predefined selection.
+		float startSpeedY = ofRandom(gameSpeed / 2) - gameSpeed;
+		float startSpeedX = (playerTurn == PlayerTurn::player1) ? gameSpeed : -gameSpeed;
+
+		// Set X speed (horizontal direction) depending on who is serving.
+		ball.cruiseAt({startSpeedX, startSpeedY});
+	}
+
+	void gamePlay(float deltaTime, float canvasWidth, float canvasHeight) {
+		p1Paddle.move(deltaTime);
+		p2Paddle.move(deltaTime);
+		p1Paddle.clampToBoundary({0, 0}, {canvasWidth, canvasHeight});
+		p2Paddle.clampToBoundary({0, 0}, {canvasWidth, canvasHeight});
+
+		ball.move(deltaTime);
+		ball.bounceHorizontalWithEdge(0, canvasHeight);
+		ball.bounceVerticalWith(p1Paddle);
+		ball.bounceVerticalWith(p2Paddle);
+	}
+
+	void checkForWin(float canvasWidth) {
+		const Edge offEdge = ball.beyondVerticalBounds(0, canvasWidth);
+
+		if (offEdge == Edge::none) return;
+
+		// Someone got a point, so the rally restarts.
+		startRally = true;
+
+		// CHECK IF PLAYER 1 SCORES POINT 
+		if (offEdge == Edge::right) {
+			++p1Score;
+			playerTurn = PlayerTurn::player1;
+		}
+
+		// CHECK IF PLAYER 2 SCORES POINT 
+		if (offEdge == Edge::left) {
+			++p2Score;
+			playerTurn = PlayerTurn::player2;
+		}
+	}
+
+	void keyPressedPvP(ofKeyEventArgs& e) {
+		if (e.key == 'w') p1Paddle.cruiseAt({0, -gameSpeed});
+		if (e.key == 's') p1Paddle.cruiseAt({0, gameSpeed});
+		if (e.key == 'i') p2Paddle.cruiseAt({0, -gameSpeed});
+		if (e.key == 'k') p2Paddle.cruiseAt({0, gameSpeed});
+	}
+
+	void keyReleasedPvP(ofKeyEventArgs& e) {
+		if (e.key == 'w' || e.key == 's') p1Paddle.cruiseAt({0, 0});
+		if (e.key == 'i' || e.key == 'k') p2Paddle.cruiseAt({0, 0});
 	}
 
 private:
 	GameType gameType;
+	PlayerTurn playerTurn;
+
+	bool startRally;
+
+	short p1Score{0}, p2Score{0};
+
+	const float paddleWidth{20.0f};
+	const float paddleHeight{100.0f};
+	const float paddleEdgeBuffer{50.0f};
+	const float ballWidth{20.0f};
+	const float ballHeight{20.0f};
+	const float gameSpeed{500.0f};
+
+	MotionSprite p1Paddle{0, 0, paddleWidth, paddleHeight, 0, 0};
+	MotionSprite p2Paddle{0, 0, paddleWidth, paddleHeight, 0, 0};
+	MotionSprite ball{0, 0, ballWidth, ballHeight, 0, 0};
 };
 
 class ofApp : public ofBaseApp {
@@ -28,21 +247,6 @@ public:
 	void update() override;
 	void draw() override;
 
-	void keyPressed(int key) override;
-	void keyReleased(int key) override;
-
 private:
-	const int canvasWidth{800}, canvasHeight{500};
-
-	bool startRally{true};
-	bool p1Serves{ofRandom(0, 100) < 50};
-
-	bool p1UpPressed{false}, p1DownPressed{false}, p2UpPressed{false}, p2DownPressed{false};
-
-	float ballXPosition{0.0f}, ballYPosition{0.0f};
-	float ballXSpeed{0.0f}, ballYSpeed{0.0f};
-
-	float p1YPosition{0.0f}, p2YPosition{0.0f};
-
-	short p1Score{0}, p2Score{0};
+	PongGame pongGame;
 };
